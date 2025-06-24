2bbaa5a522394190c291601a3beee19aca69c1b7
nilofarshaikh12 <nilofar.shaikh@bnt-soft.com>
2025-06-24 12:29:31 +0530

    performed code and unit testing to check palindrome string

---
+++ app/src/main/java/evaluationassignment/checkPalindromeString/CheckPalindromeString.java
@@ -0,0 +1,25 @@
+package evaluationassignment.checkPalindromeString;
+
+import java.util.Scanner;
+
+public class CheckPalindromeString {
+    public static int isPalindrome(String str) {
+        str = str.toLowerCase();
+        char ch[] = str.toCharArray();
+        int end = ch.length - 1;
+        for (int i = 0; i < ch.length / 2; i++, end--) {
+            if (ch[i] != ch[end]) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str1 = sc.nextLine();
+
+        System.out.println(isPalindrome(str1));
+    }
+}
---
+++ app/src/test/java/evaluationassignment/testPalindromeString/checkPalindromeStringTest.java
@@ -0,0 +1,43 @@
+package evaluationassignment.testPalindromeString;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import evaluationassignment.checkPalindromeString.CheckPalindromeString;
+
+public class checkPalindromeStringTest {
+
+    @Test
+    public void testPalindromeLowerCase() {
+        assertEquals(1, CheckPalindromeString.isPalindrome("ababa"));
+    }
+
+    @Test
+    public void testPalindromeMixedCase() {
+        assertEquals(1, CheckPalindromeString.isPalindrome("RaceCar"));
+    }
+
+    @Test
+    public void testNotPalindrome() {
+        assertEquals(0, CheckPalindromeString.isPalindrome("abc"));
+    }
+
+    @Test
+    public void testSingleCharacter() {
+        assertEquals(1, CheckPalindromeString.isPalindrome("a"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertEquals(1, CheckPalindromeString.isPalindrome(""));
+    }
+
+    @Test
+    public void testPalindromeWithSpaces() {
+        assertEquals(0, CheckPalindromeString.isPalindrome("car start"));
+    }
+
+    @Test
+    public void testPalindromeWithSpecialCharacter() {
+        assertEquals(0, CheckPalindromeString.isPalindrome("madam!"));
+    }
+}
